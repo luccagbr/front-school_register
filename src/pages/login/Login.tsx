61a6997da98d8416c6713f73272168ec1a10e361
Lucca Gabriel <luccavitalc@gmail.com>
2024-07-31 17:57:46 -0300

    feat: removed unnecessary component

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,19 +1,11 @@
 import { Box } from "@/styles/box";
-import { Modal } from "@/styles/modal";
-import { useState } from "react";
 
 export function Login() {
-    const [isStatusModal, setIsStatusModal] = useState(false);
-
     return (
         <>
             <div style={{ height: "100%", border: "1px solid red", width: "100%", position: "relative" }}>
-                <Box style={{ border: "1px solid pink" }} onClick={() => setIsStatusModal((prev) => !prev)}>
-                    Teste
-                </Box>
+                <Box style={{ border: "1px solid pink" }}>Teste</Box>
             </div>
-
-            <Modal open={isStatusModal}>Teste</Modal>
         </>
     );
 }
