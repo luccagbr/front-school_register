2fee15064db5df07809a2d3ed779b09f43df8350
Lucca Gabriel <luccavitalc@gmail.com>
2024-12-16 11:13:02 -0300

    feat: removing files

--- src/components/Dialog/Portal.tsx
+++
@@ -1,7 +0,0 @@
-import { ComponentProps } from "react";
-
-export interface PortalProps extends ComponentProps<"dialog"> {}
-
-export function Portal(props: PortalProps) {
-    return <dialog {...props} />;
-}
--- src/components/Dialog/Root.tsx
+++
@@ -1,7 +0,0 @@
-import { ComponentProps } from "react";
-
-export interface RootProps extends ComponentProps<"div"> {}
-
-export function Root(props: RootProps) {
-    return <div {...props} />;
-}
