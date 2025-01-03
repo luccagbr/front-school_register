83cd62c494bed532605ac0b3585910eda673774d
Lucca <luccavitalc@gmail.com>
2025-01-02 21:21:06 -0300

    S001-C003: Card started

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Router } from "./routers/Router";
 import { AuthContextProvider } from "./contexts/AuthContext";
 import { useEffect, useState } from "react";
 import { THEME } from "./constants/local-storage";
-import { HomePage } from "./components/HomePage";
 import { GlobalStyle } from "./styles/global";
 
 function App() {
@@ -10,8 +9,6 @@ function App() {
     const [theme, setTheme] = useState<"light" | "dark">(localTheme ?? "dark");
 
     useEffect(() => {
-        console.log(typeof localTheme);
-
         if (localTheme == null) {
             localStorage.setItem(window.btoa(THEME), theme);
         }
